8f39e2a5df24e24ea9479ad82a68773dee824f8f
holy44162 <holy44162@gmail.com>
2018-06-09 13:48:08 +0800

    add a junk line just for git testing.

--- tracker.m
+++ tracker.m
@@ -37,7 +37,7 @@ function [positions, rect_results, fps] = tracker(video_path, img_files, pos, ta
 %
 %   revised by: Yang Li, August, 2014
 %   http://ihpdep.github.io
-
+% added by Holy for git test
 
 addpath('./utility');
 temp = load('w2crs');
